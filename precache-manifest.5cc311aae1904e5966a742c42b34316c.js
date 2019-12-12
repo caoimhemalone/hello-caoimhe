1efefa7a6afa84f8c6c092f7d377315afc8b9e96
Caoimhe Malone <caoimhemalone@Caoimhes-MacBook-Air.local>
2019-12-12 13:22:53 +0000

    Updates

---
+++ precache-manifest.5cc311aae1904e5966a742c42b34316c.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfd8174522dc4fae0c572b297d33494",
+    "url": "/hello-caoimhe/index.html"
+  },
+  {
+    "revision": "a47d61e039b4981697b9",
+    "url": "/hello-caoimhe/static/css/main.8e7568bb.chunk.css"
+  },
+  {
+    "revision": "5910770bc647c1e6ef9d",
+    "url": "/hello-caoimhe/static/js/2.dfbd24c3.chunk.js"
+  },
+  {
+    "revision": "a47d61e039b4981697b9",
+    "url": "/hello-caoimhe/static/js/main.8e72bfc9.chunk.js"
+  },
+  {
+    "revision": "53a6d3d5f0554afd9a8f",
+    "url": "/hello-caoimhe/static/js/runtime-main.e34b8cee.js"
+  },
+  {
+    "revision": "2de0b9bd4d5ea535a516f91165df50fa",
+    "url": "/hello-caoimhe/static/media/bkg_1.2de0b9bd.jpg"
+  },
+  {
+    "revision": "e9af5c3eb17b8a4ae2de0f82438fa14a",
+    "url": "/hello-caoimhe/static/media/bkg_10.e9af5c3e.jpg"
+  },
+  {
+    "revision": "d2c51efa9d3b9101e1c7991d5c5f9cbc",
+    "url": "/hello-caoimhe/static/media/bkg_11.d2c51efa.jpg"
+  },
+  {
+    "revision": "ab25b9c7a988ae623d383fb28edd5dd1",
+    "url": "/hello-caoimhe/static/media/bkg_12.ab25b9c7.jpg"
+  },
+  {
+    "revision": "8a4f13fa5f3f056f267d5b1f83c11b67",
+    "url": "/hello-caoimhe/static/media/bkg_13.8a4f13fa.jpg"
+  },
+  {
+    "revision": "25e4a5952b7df357ad1a085eafec499b",
+    "url": "/hello-caoimhe/static/media/bkg_14.25e4a595.jpg"
+  },
+  {
+    "revision": "693406af63194e1b352a44d53c3d45d8",
+    "url": "/hello-caoimhe/static/media/bkg_15.693406af.jpg"
+  },
+  {
+    "revision": "727afbbebeb5c2baf8c5e968050306d9",
+    "url": "/hello-caoimhe/static/media/bkg_16.727afbbe.jpg"
+  },
+  {
+    "revision": "91cff4812d10aa07daa6109006c4d21b",
+    "url": "/hello-caoimhe/static/media/bkg_17.91cff481.jpg"
+  },
+  {
+    "revision": "025a5bbe444a674733927652e125c105",
+    "url": "/hello-caoimhe/static/media/bkg_18.025a5bbe.jpg"
+  },
+  {
+    "revision": "f6d60b5f9e12f3da040c23965c5c0b3b",
+    "url": "/hello-caoimhe/static/media/bkg_19.f6d60b5f.jpg"
+  },
+  {
+    "revision": "740670fae4106b7c3310869f8e3fa5fe",
+    "url": "/hello-caoimhe/static/media/bkg_2.740670fa.jpg"
+  },
+  {
+    "revision": "fb4289322bc984cc7ca9548c8c3cd0da",
+    "url": "/hello-caoimhe/static/media/bkg_20.fb428932.jpg"
+  },
+  {
+    "revision": "887eeba099ba485664c62026fe933f1b",
+    "url": "/hello-caoimhe/static/media/bkg_21.887eeba0.jpg"
+  },
+  {
+    "revision": "aa036a9e7ddb581b62953ee8bd82b7d8",
+    "url": "/hello-caoimhe/static/media/bkg_22.aa036a9e.jpg"
+  },
+  {
+    "revision": "98641f9e158ec55fe077741eec79bbbb",
+    "url": "/hello-caoimhe/static/media/bkg_23.98641f9e.jpg"
+  },
+  {
+    "revision": "ced1b8aad4c24586d708626254d02692",
+    "url": "/hello-caoimhe/static/media/bkg_24.ced1b8aa.jpg"
+  },
+  {
+    "revision": "f9f061b0245aa8f417b6f81b5af1e5de",
+    "url": "/hello-caoimhe/static/media/bkg_25.f9f061b0.jpg"
+  },
+  {
+    "revision": "5297413f3af18de95d3bdab37bd38deb",
+    "url": "/hello-caoimhe/static/media/bkg_26.5297413f.jpg"
+  },
+  {
+    "revision": "72882f7ee04401789c4dac15f49eedaf",
+    "url": "/hello-caoimhe/static/media/bkg_27.72882f7e.jpg"
+  },
+  {
+    "revision": "b05827587fb928c24827c1e2cc9cef18",
+    "url": "/hello-caoimhe/static/media/bkg_28.b0582758.jpg"
+  },
+  {
+    "revision": "9f39908f545864c9773450d0b3aa4bfe",
+    "url": "/hello-caoimhe/static/media/bkg_29.9f39908f.jpg"
+  },
+  {
+    "revision": "727333b41ecab09f693545ca0de32dd6",
+    "url": "/hello-caoimhe/static/media/bkg_3.727333b4.jpg"
+  },
+  {
+    "revision": "189a0cee813d6378f05ff987d2599b00",
+    "url": "/hello-caoimhe/static/media/bkg_30.189a0cee.jpg"
+  },
+  {
+    "revision": "5ee4db448b772450ed21d0db1a9c7293",
+    "url": "/hello-caoimhe/static/media/bkg_31.5ee4db44.jpg"
+  },
+  {
+    "revision": "5a1a9968bfccc84696b5ab213761b619",
+    "url": "/hello-caoimhe/static/media/bkg_4.5a1a9968.jpg"
+  },
+  {
+    "revision": "2edc19bcefefbfbffa17b34176660d94",
+    "url": "/hello-caoimhe/static/media/bkg_5.2edc19bc.jpg"
+  },
+  {
+    "revision": "904bbb781439971461aea8e35fb392d4",
+    "url": "/hello-caoimhe/static/media/bkg_6.904bbb78.jpg"
+  },
+  {
+    "revision": "c04908c23435b00a48b2aad865ef4195",
+    "url": "/hello-caoimhe/static/media/bkg_7.c04908c2.jpg"
+  },
+  {
+    "revision": "81b975a43a44694efd35c79c018970c8",
+    "url": "/hello-caoimhe/static/media/bkg_8.81b975a4.jpg"
+  },
+  {
+    "revision": "d40e8233147d0dff4e363f0e8ccb7b41",
+    "url": "/hello-caoimhe/static/media/bkg_9.d40e8233.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caoimhemalone/hello-caoimhe/precache-manifest.ffe48e6fd0ccdfcd678fcae77c5695a9.js"
+  "/hello-caoimhe/precache-manifest.5cc311aae1904e5966a742c42b34316c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/caoimhemalone/hello-caoimhe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hello-caoimhe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
