69328182789246aa61a4ca6e86480e9e366cdd15
Caoimhe <caoimhegmalone@gmail.com>
2020-04-30 21:09:43 +0100

    death countdown added

--- src/components/mainComponents/dashboard.js
+++ src/components/mainComponents/dashboard.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Clock from './time';
 import Quote from './quote';
 import Calendar from './date';
-import Weather from './weather';
+import Countdown from './days_left';
 
 function Dash() {
   return (
     <div className="dash-wrapper">
       <div className="top">
         <section className="date"><Calendar/></section>
-        <section className="weather"><Weather/></section>
+        <section className="days-left"><Countdown timeTillDate="09 02 2077, 12:00 am" timeFormat="MM DD YYYY, h:mm a" />
+</section>
       </div>
       <div className="mainInfo">
 	      <section className="time"><Clock/>
